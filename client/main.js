be4e4d8c451cb94b6019d23d2e01b9edf5f23a8d
Carlo Quinonez <carlo@studiofathom.com>
2016-10-27 23:52:12 -0700

    Mostly works. Can't pass in privateInfo yet.

--- client/main.js
+++ client/main.js
@@ -3,20 +3,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.send_data.events({
+  'click #send'(event, template) {
+  	var data = template.find('#data').value;
+    Meteor.call('sendToFusion', JSON.stringify(data));
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 
+var openurl = require('openurl').open;
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+let Api = new Restivus({
+  prettyJson: true
+});
+
+Api.addRoute('healthcheck', {
+  get: () => {
+    return 'OK';
+  }
+});
+
+Api.addRoute('submit', {
+  post: function() {
+    var data = this.bodyParams;
+    console.log('/api/submit #### data: ', data);
+    openurl('fusion360://command=open&file=/dev/null.f3d&privateInfo=' + JSON.stringify(data));
+    return data;
+  }
+})
+
+Meteor.methods({
+  sendToFusion: function(data) {
+  	console.log('Sending data: ', data);
+    openurl('fusion360://command=open&file=/dev/null.f3d&privateInfo=' + JSON.stringify(data));
+  }
+})
