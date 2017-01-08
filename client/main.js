8f0e27ae1a825e171a1cc1ec896f4d742a233ead
Carlo Quinonez <carlo@studiofathom.com>
2017-01-08 14:08:44 -0500

    Dashboard logs incoming API messages and allows manually posting a message

--- client/main.js
+++ client/main.js
@@ -1,11 +1,19 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Messages } from '/imports/messages.js';
+import { EJSON } from 'meteor/ejson';
 import './main.html';
 
-Template.send_data.events({
+Template.test_api.events({
   'click #send'(event, template) {
-  	var data = template.find('#data').value;
-    Meteor.call('sendToFusion', JSON.stringify(data));
-  },
+  	var data = EJSON.parse(template.find('#data').value);
+  	if (!data) console.error('Failed to parse JSON');
+    Meteor.call('sendToFusion', data);
+  }
 });
+
+Template.monitor_api.helpers({
+  last_messages() {
+  	return Messages.find({}, { sort: { time: -1 }}).fetch();
+  }
+});
+
