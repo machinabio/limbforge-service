ad9fa419b9a2b85825f283fc6edddee5f49c4382
Carlo Quinonez <carlo@studiofathom.com>
2017-01-09 19:53:51 -0500

    Switch to use form inputs. Added creation of multiple URLs for the different components.

--- server/main.js
+++ server/main.js
@@ -19,22 +19,89 @@ Api.addRoute('healthcheck', {
 });
 
 Api.addRoute('submit', {
-  post: function() {
-    var data = this.bodyParams;
-    console.log('/api/submit #### data: ', data);
-    var message = {};
-    message.content = EJSON.stringify(data);
-    message.time = new Date();
-    Messages.insert(message);
+  get: function() {
+    console.log('/api/submit #### data: ', this.queryParams.parameters);
+    var data = EJSON.parse(this.queryParams.parameters);
+    Messages.insert({
+      content: EJSON.stringify(data),
+      time: new Date()
+    });
+    
+    var baseUrl = 'https://s3.amazonaws.com/limbforgestls'
+
+    /// GET THE FOREARM
+    var forearmFilename = [];
+    forearmFilename.push('forearm_ebearm_');
+    forearmFilename.push(data.orientation)
+    forearmFilename.push('_C4-')
+    forearmFilename.push(data.C4);
+    forearmFilename.push('_L1-')
+    forearmFilename.push(data.L1)
+    forearmFilename.push('.stl');
+    forearmFilename = forearmFilename.join('')
+
+    var url = [];
+    url.push(baseUrl);
+    url.push('forearm/ebearm');
+    url.push(data.orientation)
+    url.push(forearmFilename)
+    url = url.join('/')
+
+    console.log('Generated forearm URL is ' + url);
+
+    // var request = HTTP.get(url);
+
+    /// GET THE TERMINAL
+    var terminalFilename = [];
+    terminalFilename.push('td_');
+    terminalFilename.push(data.TD)
+    terminalFilename.push('_')
+    terminalFilename.push(data.orientation);
+    terminalFilename.push('.stl');
+    terminalFilename = terminalFilename.join('')
+
+    var url = [];
+    url.push(baseUrl);
+    url.push('td');
+    url.push(data.orientation)
+    url.push(terminalFilename)
+    url = url.join('/')
+
+    console.log('Generated forearm URL is ' + url);
+    // var request = HTTP.get(url);
+
+    /// GET THE ADAPTOR
+    var url = [];
+    url.push(baseUrl);
+    url.push('EbeArm');
+    url.push('EbeArm_wrist_unit+v1.stl')
+    url = url.join('/')
+
+    console.log('Generated wrist URL is ' + url);
+
+    // var request = HTTP.get(url);
+
+    var response = {
+      statusCode: 200,
+      headers: {
+        'Content-Disposition' : 'attachment; filename=' + forearmFilename,
+        'Content-Type': 'application/vnd.ms-pkistl',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'x-requested-with' 
+      },
+      // body: request.content
+      body: 'request.content'
+    };
+
     // open('fusion360://command=open&file=/dev/null.f3d&privateInfo=' + JSON.stringify(data));
-    return 'ok';
+    return response;
   }
-})
+});
 
 Meteor.methods({
   sendToFusion: function(data) {
-  	console.log('Sending data: ', data);
-    HTTP.post('http://localhost:3000/api/submit', {data: data})
-    // openurl('fusion360://command=open&file=/dev/null.f3d&privateInfo=' + JSON.stringify(data));
+    console.log('Sending data: ', data);
+    HTTP.get('http://localhost:3100/api/submit', { params: {parameters: EJSON.stringify(data)}})
+      // openurl('fusion360://command=open&file=/dev/null.f3d&privateInfo=' + JSON.stringify(data));
   }
-})
+});
