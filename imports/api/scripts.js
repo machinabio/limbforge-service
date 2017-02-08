fdb2e08fac4ebd82b8569a7a4b0561c751e37a73
Carlo Quinonez <carlo@studiofathom.com>
2017-02-07 16:35:41 -0800

    works

---
+++ imports/api/scripts.js
@@ -0,0 +1,31 @@
+import { Class } from 'meteor/jagi:astronomy';
+
+const Scripts = new Mongo.Collection('scripts');
+const Script = Class.create({
+  name: 'Script',
+  collection: Scripts,
+  fields: {
+    code: {
+      type: String,
+      optional: true
+    },
+    name: {
+      type: String,
+      optional: true
+    },
+    userId: {
+      type: String,
+      optional: true
+    }
+  },
+  behaviors: {},
+  events: {},
+  secured: false
+});
+
+// var doc = {};
+// doc.userId = 'xxx';
+// doc.code = '//test';
+// Scripts.insert(doc);
+
+export default Script;
