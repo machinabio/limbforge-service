55e90df9f4906f6a25f5f0416cc16612154df4b1
Carlo Quinonez <Carlo@studiofathom.com>
2017-06-07 22:32:56 -0700

    Working on CORS for local development.

--- imports/startup/server.js
+++ imports/startup/server.js
@@ -1,3 +1,10 @@
 import './both.js';
 
 import '/imports/publications/publications.js'
+
+import { WebApp } from 'meteor/webapp'
+
+WebApp.rawConnectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "localhost");
+  return next();
+});
